8c753d9933b8f2791f11c17599490300e80d1135
poudelsubhan <poudelsubhan@gmail.com>
2017-07-31 22:41:41 -0400

    Added a split function and made sure all items from the file are added properly for computation.

--- main.cpp
+++ main.cpp
@@ -5,33 +5,50 @@
 #include <string>
 #include <sstream>
 #include <fstream>
-#include <boost/algorithm/string.hpp>
+#include <stdlib.h>
 
 using namespace std;
 
 #include "Food.h"
 
 vector<Food> foodList;
+// This vector will contain objects of type "Food"
+
+vector<string> split(string line, char c) {
+  // The split function that will split the line on the character "c".
+  vector<string> retVec;
+  stringstream ss(line);
+  string token;
+
+  while(getline(ss, token, c)) {
+    retVec.push_back(token);
+  }
+
+  return retVec;
+}
 
 bool readInFile(string filename) {
   string line;
-  vector<string> fields;
   ifstream file(filename.c_str());
+  string delim = ",";
   if (!file.is_open())
     return false;
   while (getline(file, line)) {
-    split(fields, line, is_any_of(","));
-    print fields;
-  }
-  file.close();
+    vector<string> sp = split(line, ',');
+    
+    //adding all of the items, all of these were taken from the book 'The Shredded Chief' by Michael Matthews."
+    //order of parameters: name, type, page, calories, protein (g), carbs (g), fat(g), time (min)
+    Food item = Food(sp[0], atoi(sp[1].c_str()), atoi(sp[2].c_str()), atoi(sp[3].c_str()), atoi(sp[4].c_str()), atoi(sp[5].c_str()), atoi(sp[6].c_str()), atoi(sp[7].c_str()));
+    
+    foodList.push_back(item);
+    }
   return true;
 }
 
 
 int main() {
 
-  //adding all of the items, all of these were taken from the book 'The Shredded Chief' by Michael Matthews."
-  //order of parameters: name, type, page, calories, protein (g), carbs (g), fat(g), time (min)
   bool val = readInFile("food.txt");
+  cout << foodList.size() << endl;
   
 }
