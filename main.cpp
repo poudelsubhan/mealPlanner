5e5faf6d7297222c42c0f5570d0380f8bb3aadd6
poudelsubhan <poudelsubhan@gmail.com>
2017-08-02 00:41:57 -0400

    started the merge sort algorithm

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ using namespace std;
 vector<Food> foodList;
 // This vector will contain objects of type "Food"
 
+
 vector<string> split(string line, char c) {
   // The split function that will split the line on the character "c".
   vector<string> retVec;
@@ -45,10 +46,50 @@ bool readInFile(string filename) {
   return true;
 }
 
+vector<Food> merge(vector<Food> left, vector<Food> right) {
+  vector<Food> answer;
+
+  while ((left.size() != 0) && right.size() != 0) {
+    if (left[0].retCal() <= right[0].retCal()) {
+      answer.push_back(left[0]);
+      left.erase(left.begin());
+    } else {
+      answer.push_back(right[0]);
+      right.erase(right.begin());
+    }
+  }
+
+  while (left.size() != 0) {
+    answer.push_back(left[0])
+  }
+
+
+}
+//
+vector<Food> mergeSort(vector<Food> items) {
+  int len = items.size()
+  if (len <= 1)
+    return items;
+
+  vector<Food> left;
+  vector<Food> right;
+  for (i = 0; i < len; i++) {
+    if (i < (len/2))
+      left.push_back(items[i]);
+    else
+      right.push_back(items[i]);
+  }
+
+  left = mergeSort(left);
+  right = mergeSort(right);
+
+  return merge(left, right);
+
+}
 
 int main() {
 
   bool val = readInFile("food.txt");
-  cout << foodList.size() << endl;
+  
   
 }
